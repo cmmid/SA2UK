f262f5a76cc7778925bbd44502490135c68011d1
adamkucharski <adam.kucharski@lshtm.ac.uk>
2020-12-24 21:57:27 +0000

    Update imported_cases.R

--- imported_cases.R
+++ imported_cases.R
@@ -6,7 +6,7 @@
 library(tidyverse)
 
 c.text <- function(x,sigF=3){
-  bp1=signif(c(median(x),quantile(x,0.025),quantile(x,0.975)),sigF)
+  if(is.numeric(x)){bp1=signif(x,sigF)}else{bp1=x}
   paste(bp1[1]," (95% CI:",bp1[2],"-",bp1[3],")",sep="")
 }
 
@@ -17,13 +17,14 @@ c.nume<-function(x){
 
 
 # Baseline parameters
-R <- 1.2 # Local reproduction number
+R <- 1.5 # Local reproduction number
 
 gt <- 6.8 # Generation time
 lambda_UK <- R/gt # lambda for transmission model
 
 detected_cases <- 2 # Initial reported cases
-
+detection_date <- as.Date("2020-12-12")
+  
 prop_inf_rep_ONS <- 0.6
 prop_inf_rep_REACT <- 0.3
 prop_inf_rep_mean <- 0.45
@@ -56,22 +57,33 @@ total_SA_variant_inf_mean %>% c.text()
 
 # Need solve: I(t) = a/lambda * ( exp(lambda*t) - 1 ) = total_SA_variant_infections
 
+# Estimate time since first introduction
 time_intro <- function(lambda,a,total_n){log(total_n*(lambda/a) + 1)/lambda}
 
-aa <- 2
-yy_2 <- time_intro(lambda_UK,a=2,total_SA_variant_inf_mean) # Estimate import time
-yy_4 <- time_intro(lambda_UK,a=4,total_SA_variant_inf_mean) # Estimate import time
+aa1 <- 2
+aa2 <- 4
+
+yy_2 <- time_intro(lambda_UK,a=aa1,total_SA_variant_inf_mean) # Estimate import time
+yy_4 <- time_intro(lambda_UK,a=aa2,total_SA_variant_inf_mean) # Estimate import time
 
 yy_2 %>% c.text(.,sigF=2)
 yy_4 %>% c.text(.,sigF=2)
 
-par(mfrow=c(1,2),mar=c(3.5,3.5,1,1),mgp=c(2,0.6,0),las=1)
+c((detection_date - yy_2)[3], (detection_date - yy_4)[2] ) # Range of dates of introduction
 
-plot(aa,yy,type="l",xlab="daily imported infections",ylab="time since first import",xaxs="i",yaxs="i",ylim=c(0,20))
-lines(aa,yy1,col="blue")
 
-plot(aa,yy*aa,type="l",xlab="daily imported infections",ylab="estimated total imports",xaxs="i",yaxs="i",ylim=c(0,100))
-lines(aa,yy1*aa,col="blue")
+# Estimate total imported cases up to most recent detection date
+
+(aa1*yy_2) %>% c.text(.,sigF=2)
+(aa2*yy_4) %>% c.text(.,sigF=2)
+
+# par(mfrow=c(1,2),mar=c(3.5,3.5,1,1),mgp=c(2,0.6,0),las=1)
+# 
+# plot(aa,yy,type="l",xlab="daily imported infections",ylab="time since first import",xaxs="i",yaxs="i",ylim=c(0,20))
+# lines(aa,yy1,col="blue")
+# 
+# plot(aa,yy*aa,type="l",xlab="daily imported infections",ylab="estimated total imports",xaxs="i",yaxs="i",ylim=c(0,100))
+# lines(aa,yy1*aa,col="blue")
 
 
 
