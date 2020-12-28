61537b530ff7923c405ba88a117f932cb988239c
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2020-12-28 12:19:27 +0200

    change calculation to be 7 day rolling totals, rather than aggregating to weeks.

--- fig_phylo_share_ts.R
+++ fig_phylo_share_ts.R
@@ -6,45 +6,60 @@ suppressPackageStartupMessages({
 .debug <- "~/Dropbox/SA2UK"
 .args <- if (interactive()) sprintf(c(
   "%s/inputs/nextstrain_groups_ngs-sa_COVID19-ZA-2020.12.17_metadata.tsv",
-  "comparison.png"
+  "%s/outputs/figs/phylo.rds"
 ), .debug) else commandArgs(trailingOnly = TRUE)
 
 rsa.dt <- fread(.args[1])[Country == "South Africa"][order(`Collection Data`)]
 
-plot.dt <- rsa.dt[,
-    .(.N, iso3c = "ZAF"),
-    by = .(date=round(`Collection Data`, digits = "weeks"), newvariant = `Clade`=="20C")
-  ]
+rolling.int <- 7
+var.label <- "501Y.V2"
 
-plot.dt[, total := sum(N), by=.(date, iso3c) ]
+red.dt <- rsa.dt[,
+  .N,
+  by = .(
+   date=`Collection Data`,
+   variant = fifelse(`Clade`=="20C",var.label,"other")
+  )
+][date > (as.Date("2020-10-01")-rolling.int) ]
 
-plot.dt[,
-  c("lo95","hi95") := 
-    as.data.table(t(mapply(
-      function(x, n, p=x/n) binom.test(x, n, p, conf.level = .95)$conf.int,
-      x = N, n = total
-    )))
-][,
-  c("lo50","hi50") := 
-    as.data.table(t(mapply(
-      function(x, n, p=x/n) binom.test(x, n, p, conf.level = .50)$conf.int,
-      x = N, n = total
-    )))
+fill.dt <- red.dt[, .(date = as.Date(rep(min(date):max(date), 2), origin = "1970-01-01"))]
+fill.dt[, variant := rep(c(var.label, "other"), each = .N/2)]
+
+plot.dt <- dcast(
+  red.dt[
+    fill.dt, on=.(date, variant),
+    .(date, variant, N=fifelse(is.na(N), 0, N))
+  ],
+  date ~ variant, value.var = "N"
+)
+
+plot.dt[, total := as.integer(rowSums(.SD)), .SDcols = -c("date") ]
+plot.dt[, rolling.tot := as.integer(frollsum(total, rolling.int)) ]
+plot.dt[, rolling.var := as.integer(frollsum(get(var.label), rolling.int)) ]
+
+bino <- function(ci, pos, tot) as.data.table(t(mapply(
+  function(x, n, p=x/n) binom.test(x, n, p, conf.level = ci)$conf.int,
+  x = pos, n = tot
+)))
+
+plot.dt[!is.na(rolling.var),
+        c("lo95","hi95") := bino(.95, rolling.var, rolling.tot)
+]
+plot.dt[!is.na(rolling.var),
+        c("lo50","hi50") := bino(.50, rolling.var, rolling.tot)
 ]
 
-p <- ggplot(plot.dt[newvariant == TRUE]) + aes(date) +
-  geom_ribbon(aes(ymin = lo95, ymax = hi95), alpha = 0.1) +
-  geom_ribbon(aes(ymin = lo50, ymax = hi50), alpha = 0.2) +
-  geom_line(aes(y=N/total)) +
+p.phylo <- force(ggplot(plot.dt[!is.na(rolling.var)]) + aes(date) +
+  geom_ribbon(aes(ymin = lo95, ymax = hi95), alpha = 0.1, fill = "red") +
+  geom_ribbon(aes(ymin = lo50, ymax = hi50), alpha = 0.2, fill = "red") +
+  geom_line(aes(y=rolling.var/rolling.tot), color = "red") +
   scale_x_date(
     name = NULL,
     date_breaks = "months", date_minor_breaks = "weeks",
     date_labels = "%b"
   ) +
-  scale_y_continuous("501Y.V2 Fraction") +
-  coord_cartesian(ylim = c(0, 1), xlim = c(as.Date("2020-10-01")-5, as.Date("2020-12-01")+5), expand = FALSE) +
-  theme_minimal()
+  scale_y_continuous(sprintf("%s Fraction", var.label)) +
+  coord_cartesian(ylim = c(0, 1), xlim = c(as.Date("2020-10-01"), as.Date("2021-01-01")), expand = FALSE) +
+  theme_minimal())
 
-saveRDS(p, gsub("png$","rds", tail(.args, 1)))
-
-ggsave(tail(.args, 1), p, width = 6, height = 3, units = "in", dpi = 300)
+saveRDS(p, tail(.args, 1))
