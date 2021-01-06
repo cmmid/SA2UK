8f0a90331096fd349781ebf5a97f114cd379b53b
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2021-01-06 11:45:35 +0200

    simply introductions; mix equilibrium and uniform risk assumptions

--- est_introductions.R
+++ est_introductions.R
@@ -4,7 +4,7 @@ suppressPackageStartupMessages({
 
 .debug <- c("~/Dropbox/SA2UK", "ZAF")
 .args <- if (interactive()) sprintf(c(
- # "%s/outputs/Rg.rds", #' estimated
+  "%s/inputs/yuqs/%s.rds", #' estimated
   "%s/outputs/r0/%s.rds", #' estimated
   "%s/inputs/populations.rds", #' assembled from other inputs, no estimation
   "%s/inputs/pops/%s.rds", #' assembled from other inputs, no estimation
@@ -15,24 +15,30 @@ suppressPackageStartupMessages({
   "%s/outputs/introductions/%s.rds"
 ), .debug[1], .debug[2]) else commandArgs(trailingOnly = TRUE)
 
-Rl <- readRDS(.args[1])[era == "pre", med]
-
-window_start <- readRDS(.args[6])[era == "pre", end]
+yuref <- readRDS(.args[1])[order(eqs)][which.max(eqs >= .5)]
+us <- rep(yuref[, as.numeric(.SD), .SDcols = grep("^u_", colnames(yuref))], each = 2)
+ys <- rep(yuref[, as.numeric(.SD), .SDcols = grep("^y_", colnames(yuref))], each = 2)
+Rl <- readRDS(.args[2])[era == "pre", med]
+window_start <- readRDS(.args[7])[era == "pre", end]
 
 tariso <- tail(.args, 2)[1]
-pop <- readRDS(.args[2])[iso3 == tariso]
-pars <- readRDS(.args[3])
+pop <- readRDS(.args[3])[iso3 == tariso]
+pars <- readRDS(.args[4])
 
 load("NGM.rda")
 
-#' the stable age distribution of infection in the exponential growth period == the principle eigenvector
-agedist <- cm_ss_age_distro(cm_ngm(pars)$ss) 
+#' the stable age distribution of infection in the exponential growth period
+#' == the principle eigenvector
+#' not affected by rescaling R0
+refngm <- cm_ngm(pars, us, ymod = ys)
+
+agedist <- cm_ss_age_distro(refngm$ss) 
 #' generation interval depends on age distro + model infectious period durations
-gen_int <- cm_generation_time(pars)
+gen_int <- cm_generation_time(pars, ymod = ys, ngm = refngm)
 
-ref <- readRDS(.args[4])[iso3 == tariso, .SD][date >= window_start][1:(which.max(deaths > 0)+round(gen_int))]
+ref <- readRDS(.args[5])[iso3 == tariso, .SD][date >= window_start][1:(which.max(deaths > 0)+round(gen_int))]
 
-ifr <- fread(.args[5])$ifr/100
+ifr <- fread(.args[6])$ifr/100
 # MAGIC NUMBER WARNING
 pop_vs_ifr <- 2 #' pop age categories per IFR age category
 
@@ -47,11 +53,16 @@ pd <- pop[length(agedist):.N, {
 
 pd <- pd/sum(pd)
 agedist.ext <- c(agedist[-length(agedist)], agedist[length(agedist)] * pd)
-exp.ifr <- sum(ifr.ext*agedist.ext)
-#' per actual death
-exp.infs <- 1/exp.ifr
+exp.ifr <- sum((ifr.ext*agedist.ext))
 
-idur <- sum(pars$pop[[1]]$dIa*seq(0,by=0.25,length.out = length(pars$pop[[1]]$dIa)))
+# if exp.ifr purely from initial random exposure:
+subus <- us[seq(2,by=2,length.out=8)]
+subus[9:10] <- subus[8]
+alt.exp.ifr <- sum(ifr*subus/sum(subus))
+mix.exp.ifr <- sqrt(exp.ifr*alt.exp.ifr)
+
+#' assume early transmission is a mixture
+exp.infs <- 1/mix.exp.ifr
 
 #' MAGIC NUMBER WARNING
 inf2death_dur <- 22
