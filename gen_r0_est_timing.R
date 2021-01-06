33f65805bc06168fbae06a8d9bad60c24ce21c26
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2021-01-06 11:47:49 +0200

    intervention timing matching tiers

--- gen_r0_est_timing.R
+++ gen_r0_est_timing.R
@@ -14,9 +14,9 @@ tariso <- tail(.args, 2)[1]
 #' hand specifying eras from other analyses
 eras <- data.table(
   iso3 = tariso,
-  start = as.Date(sprintf("2020-%02i-%02i",c(2,3,3,4,9,11), c(1,6,22,5,7,22))),
-  end = as.Date(sprintf("2020-%02i-%02i",c(3,3,4,5,10,12), c(5,21,4,5,21,9))),
-  era = c("censor", "pre", "transition", "post", "modification", "variant")
+  start = as.Date(sprintf("2020-%02i-%02i",c(2,3,3,4,6,11), c(1,6,22,5,1,22))),
+  end = as.Date(sprintf("2020-%02i-%02i",c(3,3,4,5,10,12), c(5,21,4,5,15,9))),
+  era = c("censor", "pre", "transition", "post", "relaxation", "variant")
 )
 
 saveRDS(eras, tail(.args, 1))
