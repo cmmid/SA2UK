d34a79717628cf10ec479654443397b5114f9056
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2021-01-08 19:18:57 +0200

    SI faceted CFR

---
+++ fig_scaled_cfr_prov.R
@@ -0,0 +1,39 @@
+
+suppressPackageStartupMessages({
+  require(data.table)
+  require(ggplot2)
+})
+
+.debug <- c("~/Dropbox/SA2UK")
+.args <- if (interactive()) sprintf(c(
+  "%s/outputs/cfrs.rds",
+  "%s/outputs/figs/ccfr.rds"
+), .debug) else commandArgs(trailingOnly = TRUE)
+
+plot.dt <- readRDS(.args[1])
+
+cfr.p <- force(ggplot(plot.dt) + aes(date, md) +
+  facet_grid(province ~ .) +
+  geom_line(aes(color = ver)) +
+  geom_ribbon(aes(fill = ver, ymin = lo, ymax = hi), alpha = 0.2) +
+  # geom_ribbon(aes(fill = ver, ymin = lo50, ymax = hi50), alpha = 0.5) +
+  coord_cartesian(
+    ylim = c(0, .075),
+    xlim = as.Date(c("2020-04-01", NA)),
+    expand = FALSE
+  ) +
+  scale_y_continuous(
+    "Case Fatality Ratio (CFR)",
+    breaks = c(0,0.025,0.05,0.075),
+    labels = function(v) sprintf("%0.2g%%", v*100)
+  ) +
+  scale_x_date(name = NULL, date_breaks = "months", date_minor_breaks = "weeks", date_labels = "%b") +
+  scale_color_manual(
+    NULL,
+    breaks = c("dCFR","cCFR","nCFR"),
+    labels = c(nCFR="naive", dCFR = "delayed", cCFR = "corrected"),
+    values = c(nCFR="orchid", dCFR = "mediumorchid", cCFR="darkorchid4"),
+    aesthetics = c("color", "fill")
+  ) + theme_minimal())
+
+saveRDS(cfr.p, tail(.args, 1))
