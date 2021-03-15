bd1eb647bcf21cba7830cf52665b82091a2ce530
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2021-03-15 12:15:07 +0000

    addressing slide change requests

--- fig_epi_overview.R
+++ fig_epi_overview.R
@@ -7,7 +7,7 @@ suppressPackageStartupMessages({
 .args <- if (interactive()) sprintf(c(
   "%s/inputs/epi_data.rds",
   .debug[2],
-  "%s/outputs/figs/epi/%s.png"
+  "%s/outputs/figs/epi_%s.png"
 ), .debug[1], .debug[2]) else commandArgs(trailingOnly = TRUE)
 
 tariso <- tail(.args, 2)[1]
@@ -29,13 +29,16 @@ mlt[, outcome := fifelse(grepl("case", variable), "cases", "deaths")]
 mlt[, measure := fifelse(grepl("roll", variable), "rolling", "raw")]
 #mlt[, variant := fifelse(grepl("var", variable), "variant", "all")]
 
-p <- ggplot(mlt) +
-  aes(date, y= value, alpha = measure) +
-  facet_grid(outcome ~ ., scale = "free_y", switch = "y", labeller = labeller(
-    outcome = function(o) sprintf("%s (new per day)", o)
-  )) +
-  geom_line() +
-  geom_blank(aes(alpha = NULL), data = data.table(date=as.Date("2020-01-22"), outcome = c("cases","deaths"), value = c(1600, 30))) +
+mlt[, dow := wday(date) ]
+
+p <- function(dt = mlt, yupper = NA) ggplot(dt) +
+  aes(date, y = value, color = outcome) +
+  # facet_grid(outcome ~ ., scale = "free_y", switch = "y", labeller = labeller(
+  #   outcome = function(o) sprintf("%s (new per day)", o)
+  # )) +
+  geom_line(data = function(dt) dt[measure == "rolling"]) +
+  geom_point(data = function(dt) dt[measure == "raw"], alpha = 0.25) +
+#  geom_blank(aes(alpha = NULL), data = data.table(date=as.Date("2020-01-22"), outcome = c("cases","deaths"), value = c(1600, 30))) +
   theme_minimal(base_size = 18) +
   theme(
     strip.placement = "outside", panel.spacing.y = unit(1, "line"),
@@ -43,16 +46,18 @@ p <- ggplot(mlt) +
   ) +
   coord_cartesian(
     xlim = as.Date(c("2020-03-01", "2021-04-01")),
-    expand = F
+    ylim = c(NA, yupper),
+    expand = F, clip = "off"
   ) +
   scale_x_date(NULL,
     date_breaks = "3 months", date_minor_breaks = "month", date_labels = "%b %Y"
   ) +
   scale_y_continuous(NULL, minor_breaks = NULL) +
-  scale_alpha_discrete(NULL)
+  scale_alpha_discrete(NULL) +
+  scale_color_manual(name = NULL, values = c(cases="dodgerblue", deaths = "firebrick"))
 
-ggsave(tail(.args, 1), p, width = 16, height = 8, dpi = 900)
+ggsave(tail(.args, 1), p(mlt[outcome == "cases" & date >= "2020-03-01"], 1600), width = 16, height = 8, dpi = 300)
 ggsave(gsub("\\.(\\w+)$","_log.\\1", tail(.args,1)),
-  p + lims(y=c(0.1, NA)) + scale_y_log10(NULL, labels = scales::label_number_si(accuracy = 0.1)),
-  width = 16, height = 8, dpi = 900
+  p(mlt[date >= "2020-03-01"]) + scale_y_log10(NULL, labels = scales::label_number_si(accuracy = 0.1)),
+  width = 16, height = 8, dpi = 300
 )  
