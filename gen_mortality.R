cfd2e42be26a49352d296c0872cb9b224b9f2f4e
Carl A. B. Pearson <blank101@gmail.com>
2021-03-24 02:33:55 -0400

    more fixes to mortality munging

--- gen_mortality.R
+++ gen_mortality.R
@@ -10,47 +10,32 @@ suppressPackageStartupMessages({
   "%s/inputs/mortality.rds"
 ), .debug[1]) else commandArgs(trailingOnly = TRUE)
 
-data(mxM)
-m_mortality <- as.data.table(mxM)[,.(
-  iso3c = countrycode(country_code, "iso3n", "iso3c"),
-  male = `2015-2020`, age
-)][!is.na(iso3c)]
-
-data(mxF)
-f_mortality <- as.data.table(mxF)[,.(
-  iso3c = countrycode(country_code, "iso3n", "iso3c"),
-  female = `2015-2020`, age
-)][!is.na(iso3c)]
-
 get_sex_data <- function(sx, covidm_age_ulim = 75, covid_m_age_w = 5) {
   age_low_lims <- seq(0, covidm_age_ulim, by=covid_m_age_w)
+  
+  # need pop to weight 75+ mortality
   pop <- as.data.table(get(data(list=list(sprintf("pop%s", sx)))[[1]]))[,.(
     iso3c = countrycode(country_code, "iso3n", "iso3c"),
     pop = `2020`*1000,
     age = as.integer(gsub("^(\\d+)[^\\d]+$","\\1",age))
-  )][
-    !is.na(iso3c),
-    pop_age(.SD, age_low_lims, "age", "pop"),
-    by=iso3c
-  ]
+  )][!is.na(iso3c)]
   
+  # get unweighted mortality
+  # going to use [1,5) mortality for infants as well,
+  # and deduct "excess" infant mortality from birthrate
   mort <- as.data.table(get(data(list=list(sprintf("mx%s", sx)))[[1]]))[,.(
     iso3c = countrycode(country_code, "iso3n", "iso3c"),
     mortality = `2015-2020`, age
-  )][!is.na(iso3c)][order(age),{
-      # merging the first two age categories
-      # don't have explicit <1 and [1,5) populations
-      # so assume the mortality in under 0 determines relative population
-      # in [1,5)
+  )][!is.na(iso3c)][order(age),
     .(
-      sex = sx, age = age[-2]
-    )},
+      age = age[-2],
+      mortality = mortality[-1]
+    ),
     by=iso3c
   ]
   
-  
-  
-  mort[pop, on=.(iso3c, age, sex)]
+  mort[pop, on=.(iso3c, age)][, sex := sx ]
 }
 
-as.data.table(get(data(popF)))[,.(sex=)]
+f <- get_sex_data("F")
+m <- get_sex_data("M")
