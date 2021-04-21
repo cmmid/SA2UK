b1d8a2fdf7d4045d83d73ceb338763112168f683
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2021-04-21 17:19:34 +0100

    update interventions timing injest

--- gen_r0_est_timing.R
+++ gen_r0_est_timing.R
@@ -2,85 +2,60 @@ suppressPackageStartupMessages({
   require(data.table)
 })
 
-.debug <- c("~/Dropbox/Covid_LMIC/All_Africa_paper", "GHA")
+.debug <- c("~/Dropbox/Covid_LMIC/All_Africa_paper", "PAK")
 .args <- if (interactive()) sprintf(c(
+  "interventions.csv",
   .debug[2],
   "%s/outputs/intervention_timing/%s.rds"
 ), .debug[1], .debug[2]) else commandArgs(trailingOnly = TRUE)
 
 tariso <- tail(.args, 2)[1]
 
-transition_era <- function(
-  dates, period, i3=tariso
-) {
-  cast.dates <- as.Date(dates)
-  data.table(
-    iso3=i3,
-    start = cast.dates[1:3],
-    end = c(cast.dates[2:3]-1, cast.dates[4]),
-    era = c("pre", "transition", "post"),
-    period = period
-  )
+dt <- fread(.args[1])[iso3 == tariso][order(start)]
+dt[era == "pre", period := 1+2*((1:.N)-1)]
+dt[era == "post", period := 1+2*((1:.N)-1)]
+
+#' assert:
+#'  - multiple pre/post eras correspond to subsequent variants
+#'  - pre first period is censoring era
+#'  - between multiple pre/post intervals are transitions
+
+#' @param dt data.table, 2 rows
+#' @return data.table, the original dt with a transition era inserted
+insert_transition_era <- function(dt) {
+  ins <- copy(dt[1,])
+  ins$start <- ins$end + 1
+  ins$end <- dt[2, start-1]
+  ins$era <- "transition"
+  rbind(dt, ins)
 }
 
-censor_era <- function(from, first) {
-  dt <- copy(first)[1]
-  dt[, era := "censor" ]
-  dt[, period := period - 1L ]
-  dt[, end := start - 1 ]
-  dt[, start := as.Date(from) ]
+prepend_censor_era <- function(dt, from) {
+  cn <- copy(dt[order(start)][1,])
+  cn$period <- cn$period - 1L
+  cn$end <- cn$start - 1L
+  cn$start <- as.IDate(from)
+  cn$era <- "censor"
+  setkeyv(rbind(cn, dt), key(dt))
 }
 
-relaxation_era <- function(first, second) {
-  ret <- copy(second)[1]
-  ret[, period := period - 1L ]
-  ret[, end := start - 1 ]
-  ret[, start := first[.N, end+1] ]
-  ret[, era := "relaxation" ]
-  ret
+insert_relaxation_eras <- function(dt, pre_variant_offset = 60) {
+  if (dt[, max(period)] > 1) {
+    ref <- dt[order(start)][which.max(period > 1) + -1:0 ]
+    ins <- copy(ref[1,])
+    ins$start <- ins$end + 1
+    ins$end <- ref[2, start - pre_variant_offset]
+    ins$period <- 2
+    ins$era <- "relaxation"
+    setkeyv(rbind(dt, ins), key(dt))
+  } else dt 
 }
 
-if (.debug[2] == "ZAF") {
-  
-  initial_intervention <- transition_era(
-    sprintf("2020-%02i-%02i", c(3,3,4,4), c(6,22,5,30)),
-    1
-  )
-
-  censor <- censor_era(sprintf("2020-%02i-%02i",2, 1), initial_intervention)
-  
-  variant_intervention <- transition_era(
-    c(
-      sprintf("2020-%02i-%02i", c(11,12,12), c(22,17,29)),
-      sprintf("2021-%02i-%02i", 1, 7)
-    ),
-    3
-  )
-  
-  relax <- relaxation_era(initial_intervention, variant_intervention)
-  
-} else if (.debug[2] == "GHA") {
-  
-  initial_intervention <- transition_era(
-    sprintf("2020-%02i-%02i", c(4,5,5,6), c(1,1,15,15)),
-    1
-  )
-  
-  censor <- censor_era(sprintf("2020-%02i-%02i",2, 1), initial_intervention)
-  
-  variant_intervention <- transition_era(
-    sprintf("2021-%02i-%02i", c(1,1,2,2), c(1,21,1,15)),
-    3
-  )
-  
-  relax <- relaxation_era(initial_intervention, variant_intervention)
-}
-
-eras <- rbind(
-  censor,
-  initial_intervention,
-  relax,
-  variant_intervention
+eras <- prepend_censor_era(
+  insert_relaxation_eras(
+    setkey(dt[, insert_transition_era(.SD), by=period ], iso3, period, start, era)
+  ),
+  from = "2020-02-01"
 )
 
 saveRDS(eras, tail(.args, 1))
