67f1f33eff52196edba6229e26e031a23b767f11
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2021-04-21 17:21:08 +0100

    WIP for parameters est changes

--- est_parameters.R
+++ est_parameters.R
@@ -1,10 +1,11 @@
 suppressPackageStartupMessages({
   require(data.table)
   require(optimization)
+  require(doParallel)
 })
 
 #' fixed stride of 20; adjust starting point
-.debug <- c("~/Dropbox/Covid_LMIC/All_Africa_paper","GHA","0021")
+.debug <- c("~/Dropbox/Covid_LMIC/All_Africa_paper","PAK","0001")
 .args <- if (interactive()) sprintf(c(
   "%s/inputs/pops/%s.rds",
   "%s/inputs/urbanization.rds",
@@ -44,16 +45,15 @@ bootstrap.dt <- readRDS(.args[6])[sample %in% fitslc][period == 1]
 day0 <- as.Date(intros.dt[, min(date)])
 intros <- intros.dt[,
   intro.day := as.integer(date - date[1])
-][, .(t=Reduce(c, mapply(rep, intro.day, infections, SIMPLIFY = FALSE))), by=sample ]
+][, .(t=Reduce(c, mapply(rep, intro.day, infections, SIMPLIFY = FALSE))), by=.(sid=sample) ]
 
-popsetup <- function(basep, urbanfraction, day0) {
+popsetup <- function(basep, day0) {
   basep$date0 <- day0
-  basep$pop[[1]]$size <- round(basep$pop[[1]]$size*urbanfraction)
   basep$pop[[1]]$dist_seed_ages <- c(rep(0,4), rep(1, 6), rep(0, 6))
   basep
 }
 
-params <- popsetup(readRDS(.args[1]), urbfrac, day0)
+params <- popsetup(readRDS(.args[1]), day0)
 
 tart <- as.numeric(tarwindow - day0)
 case.slc <- case.dt[between(date, tarwindow[1], tarwindow[2]), round(croll)]
@@ -68,11 +68,6 @@ params$time1 <- endrelax
 tms <- day0 + startpost
 relaxtms <- day0 + startrelax:endrelax
 
-tier2 <- as.Date("2020-08-15")
-
-# peakday <- case.dt[date <= "2020-10-01"][which.max(croll), date]
-# peakt <- as.numeric(peakday - day0)
-
 # load covidm
 cm_path = tail(.args, 2)[1]
 cm_force_rebuild = F;
@@ -80,21 +75,23 @@ cm_build_verbose = F;
 cm_force_shared = T
 cm_version = 2
 
-suppressPackageStartupMessages({
-  source(file.path(cm_path, "R", "covidm.R"))
-})
-
-load("NGM.rda")
-
 #' reference for all bootstrap evaluation
-scheduler <- function(large, small, symp, k, shft) {
+scheduler <- function(
+  large, small, symp,
+  relax_delay, # how long until relaxation starts
+  relax_fraction, # what will be the total relaxation (% of original value)
+  relax_period # how long does relaxation take?
+#  k, shft
+) {
   cons <- list(1-c(0, small, large, small))
   si <- list(rep(1-symp, 16))
+  
+  redtime <- relaxtms[-(1:round(relax_delay))]
   
   relaxfact <- 1-(1+exp(-k*as.numeric(relaxtms-tier2-shft)))^-1
   relaxfact <- (1-relaxfact[1]) + relaxfact
   relaxcons <- lapply(relaxfact, function(rf) 1-c(0, small, large, small)*rf)
-  relaxsi <- lapply(relaxfact, function(rf) 1-rep(symp, 16)*rf)
+#  relaxsi <- lapply(relaxfact, function(rf) 1-rep(symp, 16)*rf)
   
   list(
     list(
@@ -108,57 +105,29 @@ scheduler <- function(large, small, symp, k, shft) {
       parameter = "fIs",
       pops = numeric(),
       mode = "multiply",
-      values = c(si, relaxsi),
-      times = c(tms, relaxtms)
+      values = si, #c(si, relaxsi),
+      times = tms #c(tms, relaxtms)
     )
   )
 }
 
-pb = txtProgressBar(min = 1, max = length(fitslc), initial = 1) 
-
 ascll <- function(asc, sim.cases) -sum(dbinom(case.slc, sim.cases, asc, log = TRUE))
 
-#' TODO expand sampling
-fits.dt <- bootstrap.dt[, {
-  us <- rep(.SD[, as.numeric(.SD), .SDcols = grep("^u_",names(.SD))], each = 2)*umod
-  ys <- rep(.SD[, as.numeric(.SD), .SDcols = grep("^y_",names(.SD))], each = 2)
-  pop <- params # copy constructor
+#' seeds = intros[sample == sid, t]
+dtfun <- function(sdt, umod, pars, seeds, post) {
+  us <- rep(sdt[, as.numeric(.SD), .SDcols = grep("^u_",names(sdt))], each = 2)*umod
+  ys <- rep(sdt[, as.numeric(.SD), .SDcols = grep("^y_",names(sdt))], each = 2)
+  pop <- pars # copy constructor
   pop$pop[[1]]$y <- ys
   pop$pop[[1]]$u <- pop$pop[[1]]$u*us
-  sid <- sample
-  pop$pop[[1]]$seed_times <- intros[sample == sid, t]
-  
-  pars_int <- optim(fn = function(ps, pop) {
-    lrg <- ps[1]; sml <- ps[2]; symp <- ps[3]; k <- ps[4]; shft <- ps[5]
-    if ((lrg < sml) | (lrg < symp)) .Machine$integer.max else {
-      #' calculate reduced Rt
-      rerr <- (cm_ngm(
-        pop, contact_reductions = c(home=0, work=sml, school=lrg, other=sml),
-        fIs_reductions = symp
-      )$R0/post-1)^2
-      
-      #' project and compare to relaxation period
-      pop$schedule <- scheduler(lrg, sml, symp, k, shft)
-      est <- cm_simulate(
-        pop, 1,
-        model_seed = 42L
-      )$dynamics[
-        compartment == "cases",
-        .(value = sum(value)), by=t
-      ][between(t, tart[1], tart[2]), value]
-      #' find the best possible ascertainment prob:
-      ret <- optimize(ascll, c(1e-6, .99), sim.cases = est)$objective
-      if (is.infinite(ret) | is.na(ret)) ret <- .Machine$integer.max
-      rerr + ret/length(est)
-    }},
-    pop = pop,
+  pop$pop[[1]]$seed_times <- seeds
+  pars_int <- optim(
+    ofun, pop = pop, post = post,
     par = c(0.8, 0.25, 0.25, 0.01, 0) ,
     lower = c(0.1, 0.01, 0.01, 1e-6, -50),
     upper = c(0.9, 0.9, 0.9, 0.9, 50),
     method = "L-BFGS-B"
   )
-  
-  setTxtProgressBar(pb, .GRP)
   
   lrg <- pars_int$par[1]; sml <- pars_int$par[2]; symp <- pars_int$par[3];
   k <- pars_int$par[4]; shft <- pars_int$par[5]
@@ -178,10 +147,55 @@ fits.dt <- bootstrap.dt[, {
   pars <- c(pars_int$par, asc)
   names(pars) <- c("large", "small", "sympt", "k", "shft", "asc")
   as.list(pars)
-}, by=sample]
+}
+
+ofun <- function(ps, pop, post) {
+  lrg <- ps[1]; sml <- ps[2]; symp <- ps[3]; k <- ps[4]; shft <- ps[5]
+  if ((lrg < sml) | (lrg < symp)) .Machine$integer.max else {
+    #' calculate reduced Rt
+    rerr <- (cm_eigen_ngm(
+      pop, contact_reductions = c(home=0, work=sml, school=lrg, other=sml),
+      fIs_reductions = symp
+    )$R0/post-1)^2
+    
+    #' project and compare to relaxation period
+    pop$schedule <- scheduler(lrg, sml, symp, k, shft)
+    est <- cm_simulate(
+      pop, 1,
+      model_seed = 42L
+    )$dynamics[
+      compartment == "cases",
+      .(value = sum(value)), by=t
+    ][between(t, tart[1], tart[2]), value]
+    #' find the best possible ascertainment prob:
+    ret <- optimize(ascll, c(1e-6, .99), sim.cases = est)$objective
+    if (is.infinite(ret) | is.na(ret)) ret <- .Machine$integer.max
+    rerr + ret/length(est)
+  }
+}
+
+.cl <- makeCluster(getDTthreads())
+clusterExport(.cl, ls(), environment())
+clusterEvalQ(.cl, { 
+  require(data.table)
+  require(optimization)
+})
+
+fits.dt <- rbindlist(parLapply(.cl, X = 1:nrow(bootstrap.dt), function(i) {
+  suppressPackageStartupMessages({
+    source(file.path(cm_path, "R", "covidm.R"))
+  })
+  sdt <- bootstrap.dt[i,]
+  dtfun(sdt, sdt$umod, params, intros[sdt$sample == sid, t], sdt$post)
+}))[, sample := 1:.N ]
 
 saveRDS(bootstrap.dt[fits.dt, on = .(sample)], tail(.args, 1))
 
+# suppressPackageStartupMessages({
+#   source(file.path(cm_path, "R", "covidm.R"))
+# })
+# 
+# fits2.dt <- bootstrap.dt[fits.dt, on = .(sample)]
 # est <- rbindlist(lapply(1:nrow(fits2.dt), function(i) with(as.list(fits2.dt[i,]), {
 #   us <- rep(bootstrap.dt[i, as.numeric(.SD)*umod, .SDcols = grep("^u_",names(bootstrap.dt))], each = 2)
 #   ys <- rep(bootstrap.dt[i, as.numeric(.SD), .SDcols = grep("^y_",names(bootstrap.dt))], each = 2)
@@ -198,11 +212,11 @@ saveRDS(bootstrap.dt[fits.dt, on = .(sample)], tail(.args, 1))
 # ggplot(est) +
 #  aes(date, value, group = sample) +
 #  geom_line(color="red", alpha = 0.1) +
-#  geom_line(
-#    aes(date, cases),
-#    data = case.dt,
-#    color = "grey", inherit.aes = FALSE
-#  ) +
+#  # geom_line(
+#  #   aes(date, croll),
+#  #   data = case.dt,
+#  #   color = "grey", inherit.aes = FALSE
+#  # ) +
 #   geom_line(
 #     aes(date, croll),
 #     data = case.dt,
