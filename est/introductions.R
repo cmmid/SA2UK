8b6981f989ef26770b402c18bebab80425a147ab
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2021-06-18 14:38:35 +0100

    intros update

--- est/introductions.R
+++ est/introductions.R
@@ -26,64 +26,32 @@ source(file.path(cm_path, "R", "covidm.R"))
 
 tariso <- tail(.args, 3)[1]
 
-data(popM)[countrycode(country_code)]
+#' step 1: get IFR
+ifr <- fread(.args[6])$ifr/100
+#' because the input doesn't match the breakdown in model,
+#' we have to:
+#'  - expand it (from 10 year windows to 5 year windows)
+pop_vs_ifr <- 2 #' pop age categories per IFR age category
+ifr.ext <- rep(ifr, each = pop_vs_ifr)
+#'  - consolidate the upper end
+#'  - the upper-upper end is outside the last population window,
+#'   so need to get the relevant pop from elsewhere (wpp2019)
+popMF <- 
+  as.data.table(get(data(popM)))[countrycode(country_code, "iso3n", "iso3c") == tariso, as.numeric(`2020`)] +
+  as.data.table(get(data(popF)))[countrycode(country_code, "iso3n", "iso3c") == tariso, as.numeric(`2020`)]
+if (length(ifr.ext) < length(popMF)) {
+#' of course this population data is *also* mismatched
+  popMF[length(ifr.ext)] <- sum(popMF[length(ifr.ext):length(popMF)])
+  popMF <- popMF[1:length(ifr.ext)]
+}
 
 pop <- readRDS(.args[2])
-samples.dt <- readRDS(.args[4])[period == 1]
-
-ifr <- fread(.args[6])$ifr/100
-pop_vs_ifr <- 2 #' pop age categories per IFR age category
-ifr.ext <- rep(ifr, each = pop_vs_ifr)
-#' however, will need to consolidate upper categories
+#' now, consolidate ifr to match population categories
 consol.inds <- length(pop$pop[[1]]$size):length(ifr.ext)
+ifr.ext[consol.inds[1]] <- sum(popMF[consol.inds]*ifr.ext[consol.inds])/sum(popMF[consol.inds])
+ifr.ext <- ifr.ext[1:length(pop$pop[[1]]$size)]
 
-samples.dt[, {
-  #' umod unnecessary here - ss unaffected by R0
-  us <- rep(.SD[, as.numeric(.SD), .SDcols = grep("^u_", colnames(yuref))], each = 2)
-  ys <- rep(.SD[, as.numeric(.SD), .SDcols = grep("^y_", colnames(yuref))], each = 2)
-  #' ASSUMING: transmission largely at steady state by the time the infections which lead
-  #' to detected deaths has occurred
-  ss <- cm_eigen_ngm(pop, uval = us, yval = ys)$ss
-  #' determine the expected aggregate IFR
-
-}, by=.(sample)]
-
-Rs <- readRDS(.args[4])[era == "pre" & period == 1]
-window_start <- readRDS(.args[7])[era == "pre" & period == 1, end]
-
-pars <- readRDS(.args[4])
-
-#' the stable age distribution of infection in the exponential growth period
-#' == the principle eigenvector
-#' not affected by rescaling R0
-refngm <- cm_ngm(pars, us, ymod = ys)
-
-agedist <- cm_ss_age_distro(refngm$ss) 
-#' generation interval depends on age distro + model infectious period durations
-gen_int <- cm_generation_time(pars, ymod = ys, ngm = refngm)
-
-ref <- readRDS(.args[5])[
-  iso3 == tariso, .SD
-][1:(which.max(deaths > 0)+round(gen_int))]
-
-ifr <- fread(.args[6])$ifr/100
-# MAGIC NUMBER WARNING
-pop_vs_ifr <- 2 #' pop age categories per IFR age category
-
-ifr.ext <- rep(ifr, each = pop_vs_ifr)
-#' ASSERT: ifr.ext has more categories than agedist / pars$pop[[1]]$group_names
-#' so need to expand last category by population there, but reference population
-pd <- pop[length(agedist):.N, { 
-  tmp <- pop
-  tmp[.N-1] <- tmp[.N-1]+tmp[.N]
-  tmp[-.N]
-}]
-
-pd <- pd/sum(pd)
-agedist.ext <- c(agedist[-length(agedist)], agedist[length(agedist)] * pd)
-exp.ifr <- sum((ifr.ext*agedist.ext))
-
-# if exp.ifr purely from initial random exposure:
+# if ifr were purely from initially random, population-weighted exposure:
 subus <- us[seq(2,by=2,length.out=8)]
 subus[9:10] <- subus[8]
 alt.exp.ifr <- sum(ifr*subus/sum(subus))
@@ -92,23 +60,56 @@ mix.exp.ifr <- sqrt(exp.ifr*alt.exp.ifr)
 #' assume early transmission is a mixture
 exp.infs <- 1/mix.exp.ifr
 
-#' MAGIC NUMBER WARNING
-inf2death_dur <- 22
 
+#' that gives us IFR by age, which we can then weight according
+#' to steady-state distribution of infections in each generation
+#' 
+#' now need mean generation interval
+
+p_of_having_an_interval <- 1-cumsum(c(0, pop$pop[[1]]$dIa))
+p_infection_in_interval <- head(p_of_having_an_interval/sum(p_of_having_an_interval),-1)
+
+gen_int.mu <- (
+  sum(pop$pop[[1]]$dE*((1:length(pop$pop[[1]]$dE))-1)) + sum(p_infection_in_interval*((1:length(p_infection_in_interval))-1))
+)*pop$time_step
+
+#' get relevant cases and deaths
+ref <- readRDS(.args[5])[
+  iso3 == tariso, .SD
+][1:(which.max(deaths > 0)+round(gen_int.mu))]
+
+#' get samples
+samples.dt <- readRDS(.args[4])[period == 1]
+
+window_start <- readRDS(.args[1])[era == "pre" & period == 1, end]
 slc <- ref[which.max(deaths > 0):.N][1:(gen_int/2)][deaths > 0]
 
-#' MAGIC NUMBER WARNING
+#' MAGIC NUMBERS WARNING
+inf2death_dur <- 22
 death_underreporting <- 2.7 #' assume initially no under-reporting vs later estimate of ~2.7 for SA
 
 if (slc[.N, date] - inf2death_dur < window_start) {
-  expansion <- Rs[,{
-    gens <- cumsum(value^(0:10))
-    ind <- which.max(gens > exp.infs)-1
-    missing <- exp.infs/sum(value^(0:(ind-1)))
-    infs <- missing*value^(ind-1)
-    #' infections per OBSERVED death
+  expansion <- samples.dt[, {
+    #' umod unnecessary here - ss unaffected by R0
+    us <- rep(.SD[, as.numeric(.SD), .SDcols = grep("^u_", colnames(yuref))], each = 2)
+    ys <- rep(.SD[, as.numeric(.SD), .SDcols = grep("^y_", colnames(yuref))], each = 2)
+    #' ASSUMING: transmission largely at steady state by the time the infections which lead
+    #' to detected deaths has occurred
+    ss <- cm_eigen_ngm(pop, uval = us, yval = ys)$ss
+    #' determine the expected aggregate IFR
+    ifr.mu <- sum(ifr.ext*ss)/sum(ss)
+    #' if instead, weighted purely by proportion of population / susceptibility
+    # sus.weight <- (pop$pop[[1]]$size*us)/sum(pop$pop[[1]]$size*us)
+    # ifr.mu2 <- sum(ifr.ext*sus.weight)
+    R0 <- pre
+    mu.infections <- (1/ifr.mu)
+    gens <- cumsum(R0^(0:10))
+    # how many generations until enough infections to (eventually) have a death?
+    ind <- which.max(gens > mu.infections)-1
+    missing <- exp.infs/sum(R0^(0:(ind-1)))
+    infs <- missing*R0^(ind-1)
     .(tot.infs = infs*death_underreporting)
-  }, by=sample]
+  }, by=.(sample)]
 } else stop("not implemented")
 
 intros <- expansion[, slc[,.(
