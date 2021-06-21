f4116b9bec10a9c8b553703eec4b58fbaf8cf34e
Carl Pearson <carl.pearson@lshtm.ac.uk>
2021-06-21 14:17:57 +0100

    refine submitter to include job checking

--- submitter.sh
+++ submitter.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-echo "sbatch $1 | grep -P -o '\d+'"
-jobid=`sbatch $1 | grep -P -o '\d+'`
-echo $jobid
+if [ $# == 0 ]; then
+	squeue -u $UID
+else
+	echo "sbatch $1 | grep -P -o '\d+'"
+	jobid=`sbatch $1 | grep -P -o '\d+'`
+	echo $jobid
 
-if [ $# -gt 1 ]; then
-	shift
-	while [ "$1" != "" ]; do
-		echo "sbatch --dependency=afterok:$jobid $1 | grep -P -o '\d+'" 
-		jobid=`sbatch --dependency=afterok:$jobid $1 | grep -P -o '\d+'`
-		echo $jobid
-	shift
-	done
+	if [ $# -gt 1 ]; then
+		shift
+		while [ "$1" != "" ]; do
+			echo "sbatch --dependency=afterok:$jobid $1 | grep -P -o '\d+'" 
+			jobid=`sbatch --dependency=afterok:$jobid $1 | grep -P -o '\d+'`
+			echo $jobid
+			shift
+		done
+	fi
 fi
